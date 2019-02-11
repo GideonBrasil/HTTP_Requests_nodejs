0557f3980530500d04b8499d40db2ed3c148de15
GideonBrasil <gidxx_z@yahoo.com>
2019-02-11 10:02:03 -0700

    First commit for Printing with Buffered Strings

---
+++ HTTP_Print.js
@@ -0,0 +1,32 @@
+var https = require('https');
+// Utilize the https library to GET a given URL.
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+  
+https.get(requestOptions, function (response) {
+    var appendChunkOfData = '';
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+  
+    // the callback is invoked when a `data` chunk is received,
+    // the data is then passed into my variable appendChunksOfData
+    response.on('data', function (data) {
+      appendChunkOfData += data.length;
+      // console.log(appendChunkOfData + '\n')
+    });
+  
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(appendChunkOfData + '\n')
+      console.log('Response stream complete. \n');
+    });
+  
+  });
+
+}
+getAndPrintHTML();
